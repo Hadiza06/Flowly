eb1c9a726881e7a46d8019eb9c1e2468401399c9
Hadiza06 <hadiza9013@gmail.com>
2025-10-30 10:54:04 +0100

    Partie 4 modèles et admin

--- flowly/admin.py
+++ flowly/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from flowly.models import Message
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('content', 'user')
+    #list_filter = ('user',)
+
+
+admin.site.register(Message)
--- flowly/models.py
+++ flowly/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
 
 class Message(models.Model):
     message = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
