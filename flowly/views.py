7906dfd3c365bbb9e4bc710f6d2fac2ee34f26d0
Hadiza06 <hadiza9013@gmail.com>
2025-10-30 11:33:54 +0100

    Création de formulaire HTML et méthode POST avec Django

--- flowly/views.py
+++ flowly/views.py
@@ -1,30 +1,23 @@
+#Le fichier views gère toutes les reqêtes html
 from django.shortcuts import render
+from django.http import HttpResponse
 from datetime import datetime
+from flowly.models import Message
 
 # Create your views here.
 #request est la requête http que la fonction views doit gérer
+
 def index(request):
-    context = {
-        "messages":[
-            {
-                "content": "text",
-                "username": "CommentCoder",
-                "created_at": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            },
-    {
-                "content": "text",
-                "username": "CommentCoder",
-                "created_at": datetime.now()
-            },
-    {
-                "content": "text",
-                "username": "CommentCoder",
-                "created_at": datetime.now()
-            }
+    if request.method == "POST":
+        content = request.POST.get('content')
+        user=request.user
 
-        ]
+        Message.objects.create(message=content,user=user)
 
-    }
-
+    context = {}
+#ORM Object-Relational mapper
+    context['messages'] = Message.objects.order_by('-created_at')
+#recupérer tous les objets de la table message
     return render(request, 'index.html',context=context)
 #template_name est le fichier html qu'on va servir à l'utilisateur quand on passe dans cette fonction views
+
